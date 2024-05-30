5efffc12aa9f226ab2b1570e789e87c953801817
pires lufungula <pires00243@gmail.com>
2024-05-30 02:00:19 -0400

    Add format

--- Nreines.java
+++ Nreines.java
@@ -3,44 +3,35 @@ public class Nreines {
 
 	public static void main(String[] args) {
 
-		Echiquier echiquier = new Echiquier (4);
-		
-		
-		 
-		if (ResoudreNReine(echiquier, 0)) {
-            echiquier.printEchiquier();
-        } else {
-            System.out.println("Aucune solution n'existe pour " + 2 + " reines.");
-        }
-		
-	 
+		Echiquier echiquier = new Echiquier(4);
+
+		ResoudreNReine(echiquier, 0);
+		echiquier.printEchiquier();
 
 	}
 
-	
-	public static boolean  ResoudreNReine( Echiquier e  , int col )
-	{
+	public static boolean ResoudreNReine(Echiquier e, int col) {
 		int taille = e.getTaille();
-		
-		//si toutes les reines sont positionées
-		if(col >= taille) {
+
+		// si toutes les reines sont positionées
+		if (col >= taille) {
 			return true;
 		}
-		
+
 		// placer une reine dans chaque ligne de la colonne actuelle
-        for (int i = 0; i < taille; i++) {
-            if (e.EstPositionValide(i, col)) {
-                e.placerReine(i, col);
-                //récursivement pour placer les reines dans les colonnes col+1
-                if (ResoudreNReine(e, col + 1)) {  
-                    return true;
-                }
-               // Si positionner la reine ne mène pas à une solution, l'enlever (backtracking)
-                e.enleverReine(i, col);
-            } 
-	} 
-	
-      //Si la reine ne peut pas être positionée dans aucune ligne de cette colonne
-        return false; 
-}
+		for (int i = 0; i < taille; i++) {
+			if (e.EstPositionValide(i, col)) {
+				e.placerReine(i, col);
+				// récursivement pour placer les reines dans les colonnes col+1
+				if (ResoudreNReine(e, col + 1)) {
+					return true;
+				}
+				// Si positionner la reine ne mène pas à une solution, l'enlever (backtracking)
+				e.enleverReine(i, col);
+			}
+		}
+
+		// Si la reine ne peut pas être positionée dans aucune ligne de cette colonne
+		return false;
+	}
 }
